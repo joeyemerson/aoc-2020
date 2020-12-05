2a98b9a40823b123f8ac4a127ff231adabae0b58
Joey Emerson <joey@emerson.dev>
2020-12-05 11:20:15 -0600

    day5: getBits() added to convert each line of input to bits, then getID() just separates row and col substrings, converts those to ints, and returns the calculated id

--- 05-binary-boarding/solution.cpp
+++ 05-binary-boarding/solution.cpp
@@ -1,38 +1,36 @@
 #include "../aoc.h"
 
 int getID(const std::string &boardingPass) {
-  std::string rowText = boardingPass.substr(0, 7);
-  std::string colText = boardingPass.substr(7, 3);
-  int row = 0;
-  int col = 0;
-
-  int offset = 0;
-  while (rowText.length()) {
-    row += (rowText.back() == 'F' ? 0 : 1) << offset++;
-    rowText.pop_back();
-  }
-
-  offset = 0;
-  while (colText.length()) {
-    col += (colText.back() == 'L' ? 0 : 1) << offset++;
-    colText.pop_back();
-  }
-
+  const std::string rowBits = boardingPass.substr(0, 7);
+  const std::string colBits = boardingPass.substr(7, 3);
+  int row = std::stoi(rowBits, nullptr, 2);
+  int col = std::stoi(colBits, nullptr, 2);
   return row * 8 + col;
 }
 
+std::string getBits(const std::string &boardingPass) {
+  std::string bits;
+  for (char c : boardingPass) {
+    if (c == 'F' || c == 'L') bits += '0';
+    else bits += '1';
+  }
+  return bits;
+}
+
 int main() {
+  std::unordered_set<int> ids;
   int maxID = -1;
   int minID = std::numeric_limits<int>::max();
   int myID;
-  std::unordered_set<int> ids;
+
   std::ifstream newfile("input.txt", std::ios::in);
 
   if (newfile.is_open()) {
     std::string line;
 
     while(getline(newfile, line)) {
-      int curID = getID(line);
+      std::string bits = getBits(line);
+      int curID = getID(bits);
       if (maxID < curID) maxID = curID;
       if (curID < minID) minID = curID;
       ids.emplace(curID);
